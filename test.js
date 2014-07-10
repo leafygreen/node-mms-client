d5db10d212882a6a48d3b15c9d1a27667582427c
Dennis Kuczynski <dennis.kuczynski@gmail.com>
2014-07-10 18:04:50 -0400

    More endpoints

--- test.js
+++ test.js
@@ -117,4 +117,90 @@ var user = mms.groups("51b9361d5ae9048f0aab01f4").users().add([
   }
 });
 
+
+console.log("Attempting to list alerts in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").alerts().list(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list OPEN alerts in group");
+var users = mms.get("/groups/51b9361d5ae9048f0aab01f4/alerts?status=OPEN", function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list an alert in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").alerts("531116ece4b0b0ba5eb9fca3").info(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list backup configs in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").backupconfigs().list(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list a backup config in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").backupconfigs("51b9455ce4b0747b16f1b24d").info(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list hosts in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").hosts().list(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+console.log("Attempting to list host in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").hosts("b511939ab50eab73579ee2c52e10de52").info(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+*/
+
+console.log("Attempting to list clusters in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").clusters().list(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
+/*
+
+console.log("Attempting to list cluster in group");
+var users = mms.groups("51b9361d5ae9048f0aab01f4").clusters("51b9455ce4b0747b16f1b24d").info(function(err, resp) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(resp);
+  }
+});
+
 */
